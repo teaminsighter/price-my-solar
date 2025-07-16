8add6fe739ddf89b3b6906d0327cee8849b0f328
Insighter Digital <teaminsighter@gmail.com>
2025-07-16 14:11:26 +0000

    Change the headlines, sub headlines, and the contents with better readab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
         {gtmHeadSnippet && (
