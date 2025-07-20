bed63b24a1e58a6e7638dd695bf1c8167882bf94
Insighter Digital <teaminsighter@gmail.com>
2025-07-20 06:51:40 +0000

    please add the GTM script to the landing page head tag from the db

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
         <Script id="gtm-datalayer" strategy="afterInteractive">
